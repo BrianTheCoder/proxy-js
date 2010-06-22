c91a8be1f916d416b8d7ee88e596ba5849c4842e
brianthecoder <wbsmith83@gmail.com>
2010-06-22 13:22:43 -0500

    added http mode

--- fair_balancer.js
+++ fair_balancer.js
@@ -1,9 +1,10 @@
 var net = require('net'),
     sys = require('sys');
     
-var nodes = ['127.0.0.1:6379', '127.0.0.1:6780', '127.0.0.1:6790'];
+var nodes = ['127.0.0.1:3000', '127.0.0.1:3001', '127.0.0.1:3002', '127.0.0.1:3003', '127.0.0.1:3004'];
     
-var Fair = function(nodes){
+var Fair = function(nodes, mode){
+  var mode = mode || 'tcp';
   var queue = [];
   var server = net.createServer(function(stream){
     var log = function(str){ sys.log('(server)[' + Date.now() + '] ' + str); };
@@ -16,20 +17,14 @@ var Fair = function(nodes){
       var log = function(str){ sys.log('(client-' + node + ')[' + Date.now() + '] ' + str); };
 
       client.addListener('connect', function(){
-        log('connected');
-        if(buffer.length > 0){
-          log('replaying buffered commands');
-          buffer.forEach(function(buffer){ client.write(buffer); });
-        }
+        if(buffer.length > 0) buffer.forEach(function(datum){ client.write(datum); });
       });
 
-      client.addListener('end', function(){ 
-        client.end(); 
-      });
+      client.addListener('end', client.end);
       
       client.addListener('data', function(data){
-        log('received data forwarding to server');
         stream.write(data);
+        if(mode == 'http') stream.end();
         if(queue.length == 0) nodes.push(node);
         else client.write(queue.shift());
       });
@@ -37,7 +32,6 @@ var Fair = function(nodes){
       var oldWrite = client.write;
 
       this.write = function(data){
-        log('write('+ client.readyState +') ' + sys.inspect(data));
         if(client.readyState == 'open'){
           client.write = oldWrite;
           client.write(data);
@@ -47,10 +41,10 @@ var Fair = function(nodes){
     };
   
     stream.setEncoding('utf8');
-    stream.addListener('connect', function (){
+    stream.addListener('connect', function(){
       log('connected');
     });
-    stream.addListener('data', function (data){
+    stream.addListener('data', function(data){
       address = nodes.shift();
       log('routing to '+ address);
       if(address){
@@ -58,13 +52,13 @@ var Fair = function(nodes){
         node.write(data);
       }else queue.push(data);
     });
-    stream.addListener('end', function (){
-      log('done');
-      stream.end();
-    });
+    stream.addListener('end', stream.end);
   });
-  this.run = function(port, host){ server.listen(port, host); };
+  this.run = function(port, host){ 
+    sys.log('starting on ' + host + ':' + port);
+    server.listen(port, host); 
+  };
 };
 
-var balancer = new Fair(nodes);
-balancer.run(6380, 'localhost');
+var balancer = new Fair(nodes, 'http');
+balancer.run(3050, 'localhost');
